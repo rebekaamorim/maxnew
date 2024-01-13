e406b726e9a82e14ffe16fed75fee8687a062509
rebekaamorim <rebeka.amorim@etec.sp.gov.br>
2024-01-13 14:29:17 -0300

    ttututut

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-
+/** Comportamento navbar **/
 window.addEventListener("scroll", function(){
     var header = document.querySelector('#header')
     header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+
+/* Animação hamburger */
+const menuDiv = document.getElementById('nav_mobile')
+
+menuDiv.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    const btn = document.getElementById('btn_menu')
+    btn.classList.toggle('hamburguer')
+    menuDiv.classList.toggle('abrir')
+}
 
